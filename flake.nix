91d3b0806786d306f4217c7fbff0ed8cce7d9852
Mbarlowsg <mbarlowsg@yahoo.com>
2024-11-18 15:56:13 -0500

    added settings menu to flake

--- flake.nix
+++ flake.nix
@@ -16,6 +16,36 @@
 
     outputs = { self, nixpkgs, home-manager, ... }@inputs: 
     let 
+# ---- SYSTEM SETTINGS ---- #
+        systemSettings = {
+            system = "x86_64-linux"; # system arch
+                hostname = "nixOS"; # hostname
+                profile = "test"; # select a profile defined from my profiles directory
+                timezone = "America/Chicago"; # select timezone
+                locale = "en_US.UTF-8"; # select locale
+                bootMode = "uefi"; # uefi or bios
+                bootMountPath = "/boot"; # mount path for efi boot partition; only used for uefi boot mode
+                grubDevice = ""; # device identifier for grub; only used for legacy (bios) boot mode
+                gpuType = "nvidia"; # amd, intel or nvidia; only makes some slight mods for amd at the moment
+        };
+
+# ----- USER SETTINGS ----- #
+        userSettings = rec {
+            username = "emmet"; # username
+            name = "Emmet"; # name/identifier
+            email = "emmet@librephoenix.com"; # email (used for certain configurations)
+            dotfilesDir = "~/.dotfiles"; # absolute path of the local repo
+            theme = "io"; # selcted theme from my themes directory (./themes/)
+            wm = "hyprland"; # Selected window manager or desktop environment; must select one in both ./user/wm/ and ./system/wm/
+            # window manager type (hyprland or x11) translator
+            wmType = if ((wm == "hyprland") || (wm == "plasma")) then "wayland" else "x11";
+            browser = "firefox"; # Default browser; must select one from ./user/app/browser/
+            term = "zsh"; # Default terminal command;
+            font = "Intel One Mono"; # Selected font
+            fontPkg = pkgs.intel-one-mono; # Font package
+            editor = "nvim"; # Default editor;
+            };
+
         lib = nixpkgs.lib;
         system = "x86_64-linux";
         pkgs = nixpkgs.legacyPackages.${system};
@@ -35,6 +65,14 @@
                 modules = [
                     ./hosts/test/configuration.nix
                         inputs.home-manager.nixosModules.default
+                ];
+            };
+            auto = lib.nixosSystem {
+                system = systemSettings.system;
+                specialArgs = {inherit inputs;};
+                modules = [
+                    (./. + "/hosts" + ("/" + systemSettings.profile) + "/configuration.nix")
+                    inputs.lix-module.nixosModules.default
                 ];
             };
 

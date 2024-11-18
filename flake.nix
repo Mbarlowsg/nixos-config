b2e2650d349fafeb015f4c36e907da050acaeb2b
Mbarlowsg <mbarlowsg@yahoo.com>
2024-11-18 16:15:41 -0500

    created ability to auto load profile from settings in flake and began preparing test profile for this new modular system

--- flake.nix
+++ flake.nix
@@ -19,21 +19,21 @@
 # ---- SYSTEM SETTINGS ---- #
         systemSettings = {
             system = "x86_64-linux"; # system arch
-                hostname = "nixOS"; # hostname
-                profile = "test"; # select a profile defined from my profiles directory
-                timezone = "America/Chicago"; # select timezone
-                locale = "en_US.UTF-8"; # select locale
-                bootMode = "uefi"; # uefi or bios
-                bootMountPath = "/boot"; # mount path for efi boot partition; only used for uefi boot mode
-                grubDevice = ""; # device identifier for grub; only used for legacy (bios) boot mode
-                gpuType = "nvidia"; # amd, intel or nvidia; only makes some slight mods for amd at the moment
+            hostname = "nixOS"; # hostname
+            profile = "test"; # select a profile defined from my profiles directory
+            timezone = "America/Chicago"; # select timezone
+            locale = "en_US.UTF-8"; # select locale
+            bootMode = "uefi"; # uefi or bios
+            bootMountPath = "/boot"; # mount path for efi boot partition; only used for uefi boot mode
+            grubDevice = ""; # device identifier for grub; only used for legacy (bios) boot mode
+            gpuType = "nvidia"; # amd, intel or nvidia; only makes some slight mods for amd at the moment
         };
 
 # ----- USER SETTINGS ----- #
         userSettings = rec {
-            username = "emmet"; # username
-            name = "Emmet"; # name/identifier
-            email = "emmet@librephoenix.com"; # email (used for certain configurations)
+            username = "michael"; # username
+            name = "Michael"; # name/identifier
+            email = "mbarlowsg@yahoo.com"; # email (used for certain configurations)
             dotfilesDir = "~/.dotfiles"; # absolute path of the local repo
             theme = "io"; # selcted theme from my themes directory (./themes/)
             wm = "hyprland"; # Selected window manager or desktop environment; must select one in both ./user/wm/ and ./system/wm/
@@ -47,8 +47,7 @@
             };
 
         lib = nixpkgs.lib;
-        system = "x86_64-linux";
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = nixpkgs.legacyPackages.${systemSettings.system};
     in
     {
         nixosConfigurations = {
@@ -69,7 +68,12 @@
             };
             auto = lib.nixosSystem {
                 system = systemSettings.system;
-                specialArgs = {inherit inputs;};
+                specialArgs = {
+                    inherit inputs;
+                    inherit pkgs;
+                    inherit userSettings;
+                    inherit systemSettings;
+                };
                 modules = [
                     (./. + "/hosts" + ("/" + systemSettings.profile) + "/configuration.nix")
                     inputs.lix-module.nixosModules.default
@@ -79,9 +83,17 @@
         };
 
         homeConfigurations = {
-            michael = home-manager.lib.homeManagerConfiguration {
+            user = home-manager.lib.homeManagerConfiguration {
                 inherit pkgs;
-                modules = []; # put home.nix here
+                modules = [
+                    (./. + "/hosts" + ("/" + systemSettings.profile) + "/home.nix")
+                ];
+                extraSpecialArgs = {
+                    # pass config variables from above
+                    inherit systemSettings;
+                    inherit userSettings;
+                    inherit inputs;
+                };
             };
         };
     };
--- hosts/test/configuration.nix
+++ hosts/test/configuration.nix
@@ -2,13 +2,12 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, inputs, userSettings, systemSettings ... }:
 
 {
     imports =
         [ # Include the results of the hardware scan.
         ./hardware-configuration.nix
-        ../../modules/nixos/main-user.nix
             inputs.home-manager.nixosModules.default
         ];
 
@@ -17,7 +16,7 @@
     boot.loader.grub.device = "/dev/sda";
     boot.loader.grub.useOSProber = true;
 
-    networking.hostName = "nixos"; # Define your hostname.
+    networking.hostName = # Define your hostname.
 # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
 # Flakes setup
@@ -48,10 +47,6 @@
         LC_TIME = "en_US.UTF-8";
     };
 
-# Enable the X11 windowing system.
-# You can disable this if you're only using the Wayland session.
-# services.xserver.enable = true; - Done below
-
 # Enable the GNOME Desktop Environment.
     services.xserver = {
         enable = true;
@@ -77,31 +72,17 @@
         alsa.enable = true;
         alsa.support32Bit = true;
         pulse.enable = true;
-# If you want to use JACK applications, uncomment this
-#jack.enable = true;
-
-# use the example session manager (no others are packaged yet so this is enabled by default,
-# no need to redefine it in your config for now)
-#media-session.enable = true;
     };
-
-# Enable touchpad support (enabled default in most desktopManager).
-# services.xserver.libinput.enable = true;
 
 # User settings
-
-    main-user = {
-        enable = true;
-        userName = "michael";
-        description = "Michael";
+    users.users.${userSettings.username} = {
+        isNormalUser = true;
+        description = userSettings.name;
+        extraGroups = [ "networkmanager" "wheel" "input" "dialout" "video" "render" ];
+        packages = [];
+        uid = 1000;
     };
-    
-    home-manager = {
-        extraSpecialArgs = { inherit inputs; };
-        users = {
-            "michael" = import ./home.nix;
-        };
-    };  
+
 
 # Install firefox
     programs.firefox.enable = true;
@@ -128,39 +109,15 @@
     environment.systemPackages = with pkgs; [
 #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
         wget
-            zsh
-            nodejs
-            gcc
-            neovim
-            git
-            gh
+        zsh
+        nodejs
+        gcc
+        neovim
+        git
+        gh
+        home-manager
     ];
 
-# Some programs need SUID wrappers, can be configured further or are
-# started in user sessions.
-# programs.mtr.enable = true;
-# programs.gnupg.agent = {
-#   enable = true;
-#   enableSSHSupport = true;
-# };
-
-# List services that you want to enable:
-
-# Enable the OpenSSH daemon.
-# services.openssh.enable = true;
-
-# Open ports in the firewall.
-# networking.firewall.allowedTCPPorts = [ ... ];
-# networking.firewall.allowedUDPPorts = [ ... ];
-# Or disable the firewall altogether.
-# networking.firewall.enable = false;
-
-# This value determines the NixOS release from which the default
-# settings for stateful data, like file locations and database versions
-# on your system were taken. It‘s perfectly fine and recommended to leave
-# this value at the release version of the first install of this system.
-# Before changing this value read the documentation for this option
-# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-    system.stateVersion = "24.05"; # Did you read the comment?
+    system.stateVersion = "24.05"; # Do not change without thorough checks
 
 }

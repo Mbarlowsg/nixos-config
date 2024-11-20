9d71066fc0341c929b115d93df529b476a7cf653
Mbarlowsg <mbarlowsg@yahoo.com>
2024-11-20 15:11:16 -0500

    disabled stylix

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -9,7 +9,7 @@
         [ # Include the results of the hardware scan.
         ./hardware-configuration.nix
         ../../modules/home-manager/main-user.nix
-        ../../modules/nixos/stylix/stylix.nix
+        # ../../modules/nixos/stylix/stylix.nix
             inputs.home-manager.nixosModules.default
         ];
 
